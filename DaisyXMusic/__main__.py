6cfdb311bdb58b8ed6f54a89a92948f7f8e4a679
Technical_Hunter <85411336+Technical-Hunter@users.noreply.github.com>
2021-08-19 15:18:57 +0530

    Okay now

--- DaisyXMusic/__main__.py
+++ DaisyXMusic/__main__.py
@@ -17,6 +17,7 @@
 
 import requests
 from pyrogram import Client as Bot
+from pyrogram import idle
 
 from DaisyXMusic.config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
 from DaisyXMusic.services.callsmusic.callsmusic import run
@@ -36,3 +37,4 @@ bot = Bot(
 
 bot.start()
 run()
+idle()
